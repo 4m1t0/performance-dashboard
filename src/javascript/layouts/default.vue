154ba37f7b694247882950745d89849235d134e0
4m1t0 <fum1t0.eng@gmail.com>
2019-10-20 11:33:34 +0900

    ナビゲーターバーの追加とフォント指定

--- src/javascript/layouts/default.vue
+++ src/javascript/layouts/default.vue
@@ -1,9 +1,35 @@
 <template>
   <v-app dark>
-    <v-app-bar :clipped-left="clipped" absolute color="#121221" flat app>
+    <v-app-bar :clipped-left="clipped" fixed color="#121221" flat app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
-      <v-spacer />
     </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      color="#0091EA"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      temporary
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-content>
       <v-container>
         <nuxt />
@@ -44,6 +70,13 @@ export default {
 </script>
 
 <style>
+body {
+  background: #121221;
+}
+.v-application {
+  font-family: 'SF Pro Text', 'SF Pro Icons', 'Helvetica Neue', 'Helvetica',
+    'Arial', sans-serif;
+}
 .theme--dark.v-application {
   background: #121221;
 }
